02637cde52b8b6b5b63825075849e899e23aaa13
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-09-27 17:59:23 -0400

    fetch weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-function getWeather (cityName)
+function getWeather (cityName) {
+  //apiKey is on config.js to not push it to github
+  var apiKey = config.MY_KEY;
+  //format openweather api
+  var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+  //make a get request to url
+  fetch(apiUrl).then(function(response){
+    return response.json()
+    .then(function(data) {
+      console.log(data);
+    })
+  });
+}
+
+window.onload = function() {
+  getWeather("toronto");
+}
