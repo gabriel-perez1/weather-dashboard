645a13ead5d38e30a8e80b21a17ee3014cc8fbb4
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-10-08 18:48:57 -0400

    fixed cors issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ function getWeather (cityName) {
 
   var apiKey = 'bd528a2ef708579c5c812bf729e6e8f1';
   //format openweather api
-  //var corsURL = "https://cors-anywhere-gabriel-perez1.herokuapp.com/";
-  var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+  var corsURL = "https://cors-anywhere-gabriel-perez1.herokuapp.com/";
+  var apiUrl = corsURL + "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
   //make a get request to url
-  fetch(apiUrl, {method: 'GET', mode: 'no-cors'}).then(function(response){
+  fetch(apiUrl).then(function(response){
     return response.json()
     .then(function(data) {
       console.log(data);
@@ -22,5 +22,7 @@ const userInput = document.querySelector('#userInput');
 searchForm.addEventListener('click', event =>{
   event.preventDefault();
   let cityName = userInput.value;
+  getWeather(cityName);
   console.log(cityName)
+
 });
