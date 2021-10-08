f0bbed788fffa8c4054eb4050cc9ed714baa906c
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-10-08 18:10:35 -0400

    log user info w click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-var cityName = "toronto";
-var lastCity = JSON.parse(localStorage.getItem());
+
+//var lastCity = JSON.parse(localStorage.getItem());
 
 function getWeather (cityName) {
 
   var apiKey = 'bd528a2ef708579c5c812bf729e6e8f1';
   //format openweather api
-  var corsURL = "https://cors-anywhere-gabriel-perez1.herokuapp.com/";
-  var apiUrl = corsURL + "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+  //var corsURL = "https://cors-anywhere-gabriel-perez1.herokuapp.com/";
+  var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
   //make a get request to url
-  fetch(apiUrl).then(function(response){
+  fetch(apiUrl, {method: 'GET', mode: 'no-cors'}).then(function(response){
     return response.json()
     .then(function(data) {
       console.log(data);
@@ -16,4 +16,11 @@ function getWeather (cityName) {
   });
 }
 
-window.onload = getWeather(cityName);
+const searchForm = document.querySelector('#citySearch');
+const userInput = document.querySelector('#userInput');
+
+searchForm.addEventListener('click', event =>{
+  event.preventDefault();
+  let cityName = userInput.value;
+  console.log(cityName)
+});
